1210aeab19de26d973786f03c73662ed189f960f
sjun1116 <30386423+sjun1116@users.noreply.github.com>
2017-11-23 04:34:30 +0900

    Update ResultBase.cs

--- Models/Result/ResultBase.cs
+++ Models/Result/ResultBase.cs
@@ -5,6 +5,15 @@ namespace DotnetCoreServer.Models
     public class ResultBase{
         public string Message;
         public int ResultCode;
+        public int le;
+        public int cuE;
+        public int Mon;
+        public int st;
+        public int de;
+        public int co;
+        public int iinn;
+        public int staaaaaa;
+        public int EEEEmax;
 
     }
 }
